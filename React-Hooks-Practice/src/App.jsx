dcb8ef664366fd9ac3b18d500105c307cc1e53fb
rahulptl556 <rahulptl556@gmail.com>
2024-03-06 13:59:29 +0530

    useMemo example solved

--- React-Hooks-Practice/src/App.jsx
+++ React-Hooks-Practice/src/App.jsx
@@ -1,16 +1,40 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import './App.css'
 import Todo from './components/Todo';
 
+
 function App() {
-  const [currId,setId] = useState(1)
-  const handleClick = (id) => {
-    setId(id);
-  };
+  const [count,setCount] = useState(0);
+  const [inputValue,setInputValue] = useState(0);
+  // const handleClick = (id) => {
+  //   setId(id);
+  // };
+ 
+  function clicked(){
+    setCount(count + 1);
+  }
+
+  const ans = useMemo(()=>{
+    let sum = 0;
+    for( let i=1; i<=inputValue; i++){
+      sum += i;
+    }
+    return sum;
+  },[inputValue])
+
+  function changed(e){
+    setInputValue(e.target.value);
+  }
 
   return (
     <>
-    <div>
+    <input id='num' type="number" placeholder='Enter a number' onChange={changed}/> <br />
+
+    <div>Sum is {ans}</div>  <br />
+
+    <button onClick={clicked}>Counter ({count})</button>
+
+    {/* <div>
         <button onClick={() => handleClick(1)}>1</button>
         <button onClick={() => handleClick(2)}>2</button>
         <button onClick={() => handleClick(3)}>3</button>
@@ -18,7 +42,7 @@ function App() {
         <button onClick={() => handleClick(5)}>5</button>
         
         <Todo id={currId} />
-    </div>
+    </div> */}
     </>
   )
 }
